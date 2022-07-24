32d1c7cb6f53148cd012ae6bedb0027404336ad3
NikGlad <90272508+NikGlad@users.noreply.github.com>
2022-07-24 20:04:57 +0300

    24.07

--- main.py
+++ main.py
@@ -1,16 +1,47 @@
-# This is a sample Python script.
+import random
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+HELP = """
+help - напечатать справку по программе.
+add - добавить задачу в список (название задачи запрашиваем у пользователя).
+show - напечатать все добавленные задачи.
+random - добавлять случайную задачу на дату Сегодня"""
 
+RANDOM_TASKS = ["Записаться на курс в Нетологию", "Написать Гвидо письмо", "Покормить кошку", "Помыть машину"]
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+tasks = {
 
+}
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+run = True
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+def add_todo(date, task):
+  if date in tasks:
+      tasks[date].append(task)
+  else:
+      tasks[date] = []
+      tasks[date].append(task)
+  print("Задача ", task, " добавлена на дату ", date)
+
+while run:
+  command = input("Введите команду: ")
+  if command == "help":
+    print(HELP)
+  elif command == "show":
+    date = input("Введите дату для отображения списка задач: ")
+    if date in tasks:
+      for task in tasks[date]:
+        print('- ', task)
+    else:
+      print("Такой даты нет")
+  elif command == "add":
+    date = input("Введите дату для добавления задачи: ")
+    task = input("Введите название задачи: ")
+    add_todo(date, task)
+  elif command == "random":
+    task = random.choice(RANDOM_TASKS)
+    add_todo("Сегодня", task)
+  else:
+    print("Неизвестная команда")
+    break
+
+print("До свидания!")
